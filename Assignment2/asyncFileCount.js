18308965ecb5764ab6472f2cbd67dd0f17c7750e
justakline <50549915+justakline@users.noreply.github.com>
2024-09-16 15:40:55 -0400

    Finished with some changes to some comments and some outputs. Forgot to do asyncFileCount's count

--- Assignment2/asyncFileCount.js
+++ Assignment2/asyncFileCount.js
@@ -1,12 +1,12 @@
 var fs = require('fs')
 
 if(process.argv.length < 4){
-    console.log("not enough args, make sure you have the path and the extention file type as args")
+    console.log("Error! Not enough args, make sure you have the path and the extention file type as args")
     return
 }
 
 if(process.argv.length > 4){
-    console.log("too many args, make sure you have the path and the extention file type as args ONLY ")
+    console.log("Error! Too many args, make sure you have the path and the extention file type as args ONLY ")
     return
 }
 
@@ -14,19 +14,23 @@ if(process.argv.length > 4){
 const fileName = process.argv[2]
 const extention = process.argv[3]
 
+if(!extention.includes(".")){
+    console.log("Error! Dont forget the '.' for the file extention")
+    return
+}
+
 // Used Chat for the function that gets directories... I thought it would be like python with the os library
+// Asked "how to get a directory in js node using fs" responsed with s.readdir(directoryPath, (err, files) => {} + some other non needed stuff
 fs.readdir(fileName, (error, data) => {
     if(error){
-        console.log(error)
-        console.log("File probably not found or the wrong name")
-        console.log("If the folder name has any spaces, but the path in quotes")
+        console.log("Error! File probably not found or the wrong name")
+        console.log("If the folder name has any spaces, put the path in quotes")
         return
     }
-    console.log(data)
 
     var filteredData = []
     for (item of data){
-        // Skip anything that does not inlcude a .
+        // Skip anything that does not inlcude a . like folders
         if(!item.includes(".")){
             continue
         }
@@ -39,7 +43,7 @@ fs.readdir(fileName, (error, data) => {
         }
     }
 
-    console.log(filteredData)
+    console.log("The number of files with the " + extention + " extention in " + fileName + " is " + filteredData.length)
 
 })
 
--- Assignment2/reddit.js
+++ Assignment2/reddit.js
@@ -2,14 +2,16 @@ const fs = require('fs')
 const lo = require('lodash')
 
 // Used chat to explain all of the lo functions in a sussinct way
-// Also for general syntax questions
+// "Asked what are the key functions in LoDash and what do they do"
+// It responded with a list of them, each one with general parameters, and a quick explantion... around 20 different functions
+
 if(process.argv.length < 3){
-    console.log("not enough args, make sure you have the path ")
+    console.log("Error! Not enough args, make sure you have the path ")
     return
 }
 
 if(process.argv.length > 3){
-    console.log("too many args, make sure you have the path ONLY ")
+    console.log("Error! Too many args, make sure you have the path ONLY ")
     return
 }
 
@@ -18,27 +20,27 @@ var j;
  fs.readFile(fileName,(error, data)=> {
 
     if(error){
-        console.error(error)
+        console.log("Error! File not found")
         return
     }
 
     // Used chat to diagnose why data.toJSON was giving a buffer for all of the items
+    // "Why does data.toJSON give a buffer object and not a JSON object" ... it respponded mentioning that I probably wanted JSON.parse instead
     j = JSON.parse(data)
-    console.log('Part 1 \n')
-    let arrayOfUsers = lo.forEach(j, (item) => {
-        console.log(lo.values(item)[0]);
-    });
-    
-    console.log('\n\nPart 2 \n')
+    console.log('Part 1')
+    array = lo.map( j, 'username')
+    console.log(array)
+   
+    console.log('\n\nPart 2')
     console.log(lo.groupBy(j, "username"))
     
-    console.log('\n\nPart 3 \n')
+    console.log('\n\nPart 3')
     console.log(lo.keys(lo.groupBy(j, "username")))
 
 
 
 
-    console.log('\n\nPart 4 \n')
+    console.log('\n\nPart 4 ')
     let sorted = lo.sortBy(j, "username")
     console.log(sorted)
     
